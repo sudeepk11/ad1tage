0d3e7a3d6eb2baca8adaae3fa76697018d93b4f1
sudeepk11 <79464046+sudeepk11@users.noreply.github.com>
2023-10-30 01:37:29 +0530

    L

--- components/LandingPage/Banner.tsx
+++ components/LandingPage/Banner.tsx
@@ -8,22 +8,21 @@ const Banner = () => {
   return (
     <div className="grid lg:grid-cols-2 grid-cols-1 content-center items-center h-fit my-20">
       <div className="flex flex-col justify-center text-black md:px-10 lg:order-1 order-2">
-        <h1
-          className={`md:text-[35px] text-[30px] font-semibold md:leading-[64px]  ${josefin.className}`}
-        >
-          Peace of mind: Find Nearby Services with Ease!
-        </h1>
-        <p className="text-lg mb-[18px] text-justify font-light">
-          Discover personalized and reliable solutions for a vibrant and fulfilling senior lifestyle.
-        </p>
+        <div className="text-center mb-8">
+          <h1 className="text-3xl font-semibold text-primary mb-2">Peace of Mind</h1>
+          <p className="text-lg text-center font-light mb-4">
+            Discover personalized and reliable solutions for a vibrant and fulfilling senior lifestyle.
+          </p>
+        </div>
 
-        <h1
-          className={`md:text-[35px] text-[30px] font-semibold md:leading-[64px]  ${josefin.className}`}
-        >
-          Empowering Seniors: Your gateway to a life enriched with convenience and care.
-        </h1>
-
+        <div className="text-center">
+          <h1 className="text-3xl font-semibold text-primary mb-2">Empowering Seniors</h1>
+          <p className="text-lg text-center font-light">
+            Your gateway to a life enriched with convenience and care.
+          </p>
+        </div>
       </div>
+
       {/* Center the image in its column */}
       <div className="flex justify-center lg:order-2 order-1">
         <Image
